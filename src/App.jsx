44e4bad8df55b1a45848edd28e8f8b2a8d9104d0
GasinskiN <gasinskinorman@gmail.com>
2023-03-27 12:14:13 +0200

    Make an app function that returns the Header Note and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
 
-// function App(){
-//     return
 
-// }
+function App(){
+    return (
+        <div>
+            <Header />
+            <Note 
+                title="This is a title"
+                content="This is the content"
+            />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
