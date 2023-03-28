acb4aa41f108c21a8f28f823873af34d00aec959
GasinskiN <gasinskinorman@gmail.com>
2023-03-28 21:59:33 +0200

    Render note component with a map

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "./notes";
+
 
 
 function App(){
     return (
         <div>
             <Header />
-            <Note 
-                title="This is a title"
-                content="This is the content"
-            />
+            {notes.map(component => (<Note
+                key = {component.key}
+                title = {component.title}
+                content = {component.content}
+             />
+            ))}
+
             <Footer />
         </div>
     )
