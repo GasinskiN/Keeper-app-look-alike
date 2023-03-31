59648c96e0a5133cbd85084f2af64450999c9458
GasinskiN <gasinskinorman@gmail.com>
2023-03-31 17:48:20 +0200

    Add delete note functionality and properly add key to Note

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,29 @@ function App(){
     
     const [notes, setNotes] = useState([{title: "This is a title", post: "This is the content"}, {title: "Hey", post: "there"}]);
 
-    function updateNotes(note, e){
+    function updateNotes(note){
         setNotes(prevValue => {
             return [...prevValue, note]
         })
+    }
+
+    function deleteNote(noteID){
+        setNotes(prevValue => {
+            return prevValue.filter((note, index) => index !== noteID)
+            
+        });
     }
     
     return (
         <div>
             <Header />
             <CreateArea giveNote={updateNotes}/>
-            {notes.map(component => (<Note
-                key = {component.key}
+            {notes.map((component, index) => (<Note
+                key = {index}
+                id = {index}
                 title = {component.title}
                 content = {component.post}
+                getNote = {deleteNote}
              />
             ))}
 
