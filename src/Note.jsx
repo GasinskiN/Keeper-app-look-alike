1128d82fa9d88b34faa905a9bdf61d9eb11eb2e6
GasinskiN <gasinskinorman@gmail.com>
2023-04-04 22:16:06 +0200

    Add material delete icon

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import IconButton from '@mui/material/IconButton';
 
 function Note(props){
     return (
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button onClick={()=>{props.getNote(props.id)}}>DELETE</button>
+            <IconButton onClick={()=>{props.getNote(props.id)}}><DeleteOutlineIcon /></IconButton>
         </div>
     );
 }
