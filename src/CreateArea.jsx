f02a451f8cb70b1fabcb273f752778d65b39e53a
GasinskiN <gasinskinorman@gmail.com>
2023-04-04 22:17:15 +0200

    Add conditional rendering for create note area and change the button to material icon

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,7 +1,16 @@
 import React, {useState} from "react";
-
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props){
+
+    const [isExpanded, setExpand] = useState(false);
+
+    function expand(){
+        setExpand(true);
+    }
+
     const [content, setContent] = useState({
         title: "",
         post: ""
@@ -22,11 +31,28 @@ function CreateArea(props){
 
     return (
     <div>
-    <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} name="title" value={content.title} placeholder="Title"></input>
-        <textarea onChange={handleChange} name="post" value={content.post} placeholder="Content" rows={3}></textarea>
-        
-        <button onClick={() => {props.giveNote(content); setContent({title: "", post:""})}}  >Add</button>
+    <form className="create-note" onSubmit={handleSubmit}>
+        <input 
+        onClick={expand} 
+        onChange={handleChange} 
+        name="title" 
+        value={content.title} 
+        placeholder="Title">
+        </input>
+        {isExpanded &&
+        <Zoom in={isExpanded}>
+            <textarea
+            onChange={handleChange}
+            name="post" value={content.post}
+            placeholder="Content"
+            rows={3}>
+            </textarea>
+        </Zoom>}
+        <Zoom in={isExpanded}>
+        <IconButton 
+        onClick={() => {props.giveNote(content); setContent({title: "", post:""})}}  ><AddIcon />
+        </IconButton>
+        </Zoom>
     </form>
     </div>
     );
